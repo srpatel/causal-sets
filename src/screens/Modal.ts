c8a11e09f27cc85a5aeee732094834aeb67b42da
sunil <sunil@xikka.com>
2024-07-02 14:44:14 +0200

    add rounded rect to modal

--- src/screens/Modal.ts
+++ src/screens/Modal.ts
@@ -1,9 +1,11 @@
 import * as PIXI from "pixi.js";
 import Screen from "./Screen";
 import App from "@/App";
+import Colour from "@/utils/Colour";
 
 export default class Modal extends Screen {
   background = PIXI.Sprite.from(PIXI.Texture.WHITE);
+  panel: PIXI.NineSliceSprite;
   constructor() {
     super();
 
@@ -17,11 +19,19 @@ export default class Modal extends Screen {
     this.addChild(this.background);
 
     // Modal rectangle
+    this.panel = new PIXI.NineSliceSprite(PIXI.Texture.from("roundedrect.png"));
+    this.panel.tint = Colour.SPACETIME_BG;
+    this.addChild(this.panel);
+    this.panel.width = 650;
+    this.panel.height = 950;
   }
   onSizeChanged() {
     this.background.position.set(0, 0);
     this.background.width = this.screenWidth;
     this.background.height = this.screenHeight;
+
+    this.panel.x = (this.screenWidth - this.panel.width) / 2;
+    this.panel.y = (this.screenHeight - this.panel.height) / 2;
   }
   onAddedToStage(stage: PIXI.Container) {}
   onRemovedFromStage(stage: PIXI.Container) {}
